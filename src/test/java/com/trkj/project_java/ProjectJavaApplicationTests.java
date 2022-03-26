ffaab03f84df170176287d4f2eef59b5a4afc692
周刘奇 <1379576648@qq.com>
2022-03-26 22:23:13 +0800

    init

---
+++ src/test/java/com/trkj/project_java/ProjectJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trkj.project_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
