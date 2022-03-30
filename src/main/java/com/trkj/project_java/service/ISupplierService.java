152388e3eac27b366d28c40d06053921e5d28291
袁可颖 <3043949730@qq.com>
2022-03-30 20:55:59 +0800

    完善

--- src/main/java/com/trkj/project_java/service/ISupplierService.java
+++ src/main/java/com/trkj/project_java/service/ISupplierService.java
@@ -3,6 +3,8 @@ package com.trkj.project_java.service;
 import com.trkj.project_java.entity.Supplier;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
  * <p>
  *  服务类
@@ -12,5 +14,11 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2022-03-30
  */
 public interface ISupplierService extends IService<Supplier> {
+
+    /**
+     * 查询供应商
+     * @return
+     */
+    List<Supplier> selectSupplier();
 
 }
