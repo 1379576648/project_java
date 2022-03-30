ee4c1bb472ecef7bd37567612e8cbb6731fcd686
yourenA12 <3092079817@qq.com>
2022-03-30 20:45:27 +0800

    yourenA

--- src/main/java/com/trkj/project_java/controller/OtheroutstockController.java
+++ src/main/java/com/trkj/project_java/controller/OtheroutstockController.java
@@ -1,6 +1,7 @@
 package com.trkj.project_java.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/otheroutstock")
 public class OtheroutstockController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello";
+    }
 
 }
