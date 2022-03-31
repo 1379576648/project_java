c5e0831ce310de9a9354eb14afdb481cbb7ebf07
达鸭 <2212511473@qq.com>
2022-03-31 11:02:07 +0800

    wjh

--- src/main/java/com/trkj/project_java/controller/CommodityController.java
+++ src/main/java/com/trkj/project_java/controller/CommodityController.java
@@ -1,6 +1,10 @@
 package com.trkj.project_java.controller;
 
 
+import com.trkj.project_java.entity.Commodity;
+import com.trkj.project_java.service.ICommodityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +20,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/commodity")
 public class CommodityController {
+
+    @Autowired
+    public ICommodityService iCommodityService;
+
+    public String add (@RequestBody Commodity commodity){
+        if (iCommodityService.appcommod(commodity) >=1){
+            System.out.println("成功");
+            return "成功";
+
+        }
+        System.out.println("失败");
+        return "失败";
+    }
+
+
+
+
 
 }
