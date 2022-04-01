9fcbed9400767cad4f8d25051544a81260055039
MsMsShi <2398646027@qq.com>
2022-04-01 11:43:17 +0800

    商品表分页查询

--- src/main/java/com/trkj/project_java/controller/CommodityController.java
+++ src/main/java/com/trkj/project_java/controller/CommodityController.java
@@ -1,13 +1,14 @@
 package com.trkj.project_java.controller;
 
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.trkj.project_java.entity.Commodity;
 import com.trkj.project_java.service.ICommodityService;
+import com.trkj.project_java.vo.AjaxResponse;
+import org.aspectj.weaver.loadtime.Aj;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -33,9 +34,36 @@ public class CommodityController {
         System.out.println("失败");
         return "失败";
     }
+    /*
+    * 分页查询商品表
+    * */
+    @GetMapping("/selectPageC")
+    public AjaxResponse select(@RequestParam("currentPage") int currenPage, @RequestParam("pagesize") int pagesize,@RequestParam("input") String commodityName){
+        Page<Commodity> page = new Page<>(currenPage, pagesize);
+        return AjaxResponse.success(iCommodityService.selectPage(page,commodityName));
+    }
+    /*
+    * 根据id查询商品表
+    * */
+    @GetMapping("/selectById/{id}")
+    public Commodity selectById(@PathVariable("id") int id){
+        return iCommodityService.selectId(id);
+    }
 
-
-
+    /*
+    * 根据id修改商品表
+    * */
+    @PostMapping("/updata")
+    public int updata(@RequestBody Commodity commodity){
+        return iCommodityService.updata(commodity);
+    }
+    /*
+    * 模糊查询商品名称
+    * */
+    @GetMapping("/selectName/{commodityName}")
+    public Commodity selectName(@PathVariable("commodityName") String commodityName){
+        return iCommodityService.selectName(commodityName);
+    }
 
 
 }
--- src/main/java/com/trkj/project_java/service/ICommodityService.java
+++ src/main/java/com/trkj/project_java/service/ICommodityService.java
@@ -1,5 +1,8 @@
 package com.trkj.project_java.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.trkj.project_java.entity.Commodity;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -13,5 +16,12 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface ICommodityService extends IService<Commodity> {
      int appcommod (Commodity commodity);
-
+     //分页查询商品表
+     IPage<Commodity> selectPage(Page<Commodity> page, String name);
+     //根据id查询商品表
+     Commodity selectId(int id);
+     //商品表修改
+     int updata(Commodity commodity);
+     //模糊查询
+     Commodity selectName(String commodityName);
 }
