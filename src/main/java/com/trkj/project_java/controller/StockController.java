0ce5ffeabaa25caeb52be5bd9d8e91bdc0987aea
147258qwer <2940934174@qq.com>
2022-04-01 14:49:04 +0800

    aaa

--- src/main/java/com/trkj/project_java/controller/StockController.java
+++ src/main/java/com/trkj/project_java/controller/StockController.java
@@ -3,6 +3,7 @@ package com.trkj.project_java.controller;
 
 import com.trkj.project_java.config.Result;
 import com.trkj.project_java.service.IStockService;
+import com.trkj.project_java.service.IStockvoService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
