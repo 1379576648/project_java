376497ff48dabab9c4543bd2bf5a6a27eae220fb
yangashu <2503280042@qq.com>
2022-04-02 11:15:05 +0800

    操

---
+++ src/main/java/com/trkj/project_java/config/dto/service/impl/LoginServiceImpl.java
@@ -0,0 +1,45 @@
+package com.trkj.project_java.config.dto.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.trkj.project_java.config.Result;
+import com.trkj.project_java.config.dto.service.LoginService;
+import com.trkj.project_java.entity.Power;
+import com.trkj.project_java.entity.Staff;
+import com.trkj.project_java.mapper.PowerMapper;
+import com.trkj.project_java.mapper.StaffMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class LoginServiceImpl implements LoginService {
+    @Autowired
+    private StaffMapper staffMapper;
+    @Autowired
+    private PowerMapper powerMapper;
+
+    @Override
+    public Result login(Staff staff) {
+        Staff user = staffMapper.selectOne(new LambdaQueryWrapper<Staff>().eq(Staff::getStaffName, staff.getStaffName()).and(i -> i.eq(Staff::getStaffPass, staff.getStaffPass())));
+        Map<String,Object> map=new HashMap<>();
+        if (!Objects.isNull(user)) {
+            List<Power> powers=powerMapper.findByStaffId(user.getStaffId());
+            List<Power> powerList=new ArrayList<>();
+            powers.forEach(item->{
+                powers.forEach(e->{
+                    if (item.getPowerId().equals(e.getPowerPid())){
+                        item.getPowers().add(e);
+                    }
+                });
+                if(item.getPowerPid().equals(0)){
+                    powerList.add(item);
+                }
+            });
+            map.put("user",user);
+            map.put("menus",powerList);
+           return Result.success(map);
+        }
+        return  null;
+    }
+}
--- src/test/java/com/trkj/project_java/ProjectJavaApplicationTests.java
+++ src/test/java/com/trkj/project_java/ProjectJavaApplicationTests.java
@@ -1,7 +1,10 @@
+/*
 package com.trkj.project_java;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class ProjectJavaApplicationTests {
@@ -9,5 +12,11 @@ class ProjectJavaApplicationTests {
     @Test
     void contextLoads() {
     }
+    public static void main(String[] args) {
+        PasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
+        String encode = passwordEncoder.encode("123456");
 
+        System.out.println(encode+"\n"+passwordEncoder.matches("123456",encode));
+    }
 }
+*/
