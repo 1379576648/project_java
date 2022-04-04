9355b6b8a601c06c1ad70216024417323f59fa5d
刘耀华 <1489086370@qq.com>
2022-04-04 10:13:16 +0800

    更新

--- src/main/java/com/trkj/project_java/service/ISalescheduleService.java
+++ src/main/java/com/trkj/project_java/service/ISalescheduleService.java
@@ -2,15 +2,21 @@ package com.trkj.project_java.service;
 
 import com.trkj.project_java.entity.Saleschedule;
 import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * <p>
- *  服务类
+ *  销售明细
  * </p>
  *
  * @author 沈杨卓
  * @since 2022-03-30
  */
+@Service
 public interface ISalescheduleService extends IService<Saleschedule> {
+
+
+
 
 }
