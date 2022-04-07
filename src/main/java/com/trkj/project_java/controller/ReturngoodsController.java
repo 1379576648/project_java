657be8f683b0232ddc1088db05f96d994d766f8c
“tlj203692214” <203692214@qq.com>
2022-04-07 09:14:03 +0800

    拉下来修改的退货数据

--- src/main/java/com/trkj/project_java/controller/ReturngoodsController.java
+++ src/main/java/com/trkj/project_java/controller/ReturngoodsController.java
@@ -6,13 +6,17 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.trkj.project_java.config.Result;
 import com.trkj.project_java.entity.Returngoods;
+import com.trkj.project_java.entity.Supplier;
 import com.trkj.project_java.mapper.ReturnGoodsVoMapper;
+import com.trkj.project_java.mapper.SupplierMapper;
 import com.trkj.project_java.pojovo.ReturnGoodsVo;
 import com.trkj.project_java.service.IReturngoodsService;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * <p>
@@ -31,6 +35,9 @@ public class ReturngoodsController {
     @Autowired
     private IReturngoodsService returngoodsService;
 
+    @Autowired
+    private SupplierMapper supplierMapper;
+
     /**
      * 查询已出库的数据
      *
@@ -43,8 +50,8 @@ public class ReturngoodsController {
             , @RequestParam("supplierName") String shopName) {
         Page<ReturnGoodsVo> page1 = new Page<>(page, size);
         QueryWrapper<ReturnGoodsVo> queryWrapper = new QueryWrapper<>();
-        queryWrapper.like("s.SUPPLIER_NAME", shopName)
-                .like("c.COMMODITY_NAME", shopName)
+        queryWrapper
+                .and(wrapper->wrapper.like("s.SUPPLIER_NAME", shopName).or().like("c.COMMODITY_NAME", shopName))
                 .eq("RETURNGOODS_STATE", 1);
         IPage<ReturnGoodsVo> returnGoodsVoIPage = returnGoodsVoMapper.selectAll(page1, queryWrapper);
         return returnGoodsVoIPage;
@@ -59,12 +66,12 @@ public class ReturngoodsController {
      * @return
      */
     @GetMapping("/notDelivered")
-    public IPage<ReturnGoodsVo> notDelivered(@RequestParam("currentPage") int page, @RequestParam("pageSize") int size,
-                                             @RequestParam("supplierName") String shopName) {
+    public IPage<ReturnGoodsVo> notDelivered(@RequestParam("page") int page, @RequestParam("size") int size,
+                                             @RequestParam("supplierName2") String shopName) {
         Page<ReturnGoodsVo> page1 = new Page<>(page, size);
         QueryWrapper<ReturnGoodsVo> queryWrapper = new QueryWrapper<>();
-        queryWrapper.like("s.SUPPLIER_NAME", shopName)
-                .like("c.COMMODITY_NAME", shopName)
+        queryWrapper
+                .and(wrapper->wrapper.like("s.SUPPLIER_NAME", shopName).or().like("c.COMMODITY_NAME", shopName))
                 .eq("RETURNGOODS_STATE", 2);
         IPage<ReturnGoodsVo> returnGoodsVoIPage = returnGoodsVoMapper.selectAll(page1, queryWrapper);
         return returnGoodsVoIPage;
@@ -75,6 +82,7 @@ public class ReturngoodsController {
      * 修改未出库的状态
      */
     @PostMapping("/updateReturnGoods")
+
     public Result updateReturnGoods(@RequestBody Returngoods returngoods) {
         return returngoodsService.updateReturnGoods(returngoods);
     }
@@ -89,6 +97,35 @@ public class ReturngoodsController {
         return  returnGoodsVoMapper.selectReturnGoodsById(queryWrapper);
     }
 
+    /**
+     * 查询供应商信息
+     */
+    @GetMapping("/selectSupplier")
+    public Result selectSuppliers(){
+        List<Supplier> suppliers = supplierMapper.selectList(null);
+        return Result.success(suppliers);
+    }
+
+    /**
+     *添加退货详情
+     * @param returngoods
+     * @return
+     */
+    @PostMapping("insertReturnGoods/{id}")
+    public Result insert(@PathVariable("id") int id,@RequestBody Returngoods returngoods){
+        return returngoodsService.insertReturnGoods(returngoods,id);
+    }
+
+    /**
+     * 查询退货商品信息
+     * @param id
+     * @return
+     */
+    @GetMapping("/selectReturnCommodity/{id}")
+    public Result selectReturnCommodity(@PathVariable("id")int id){
+        List<ReturnGoodsVo> returnGoodsVo = returnGoodsVoMapper.selectReturnCommodity(id);
+        return Result.success(returnGoodsVo);
+    }
 
 
 }
